5a3bc01fc4584fba0f81d7ea992b3732e2be2850
Dhairya Bhadani <dhairyabhadani8092@gmail.com>
2025-10-26 10:32:27 +0530

    Added HelloWorld.java - my first java program

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+package JavaPractice;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Dhairya! Your journey to Java Backend has begun 🚀");
+    }
+}
