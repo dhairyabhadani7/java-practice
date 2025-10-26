85fc02b1bc7ac5c2053eb5766db449a5140d6c21
Dhairya Bhadani <dhairyabhadani8092@gmail.com>
2025-10-26 15:38:07 +0530

    Added UserInput.java - basic calculator using Scanner and switch-case

---
+++ UserInput.java
@@ -0,0 +1,46 @@
+package JavaPractice;
+
+import java.util.Scanner;
+
+public class UserInput {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number");
+        double num1=sc.nextDouble();
+        System.out.println("Enter the second number");
+        double num2=sc.nextDouble();
+        System.out.println("Choose the operators +, -, * and /");
+        String operator=sc.next();
+
+        switch (operator) {
+            case "+":
+                   System.out.println(num1+num2);
+                
+                break;
+            case "-":
+                  System.out.println(num1-num2);
+                
+                break;
+            case "*":
+                   System.out.println(num1*num2);
+                
+                break;
+            case "/":
+                   if(num2!=0){
+                       System.out.println(num1/num2);
+                   }
+                   else{
+                    System.out.println("Cannot divide by zero!");
+
+                    sc.close();
+                    return;
+                   }
+                break;
+            default:
+              System.out.println("Invalid operator selected ");
+                sc.close();
+                return;
+        }
+    }
+}
